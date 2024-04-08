887f87e393a51b5b4e7378aa4d7e5eb79ecd7e2d
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2024-04-08 22:49:46 +0000

    dont send empty messages to the server

--- wallofhope.js
+++ wallofhope.js
@@ -15,6 +15,9 @@ fetch("https://puzzled-jersey-bear.cyclic.app/get_approved_msgs").then(response 
 function postMessage() {
 	// Get the message from the input
 	const message = document.getElementById("message").value;
+	if (message === "") {
+		return;
+	}
 	fetch("https://puzzled-jersey-bear.cyclic.app/addmsg", {
 		method: "POST",
 		headers: {
@@ -26,8 +29,17 @@ function postMessage() {
 	}).then(response => {
 		if (response.ok) {
 			// If the response is ok, add the message to the wall
-			alert("Message has been submitted and is awaiting approval!");
+
+			// Get the snackbar DIV
+			var x = document.getElementById("snackbar");
+
+			// Add the "show" class to DIV
+			x.className = "show";
+
+			// After 3 seconds, remove the show class from DIV
+			setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 		}
+
 	});
 	// location.reload();
 }
